5098c2fef7bb6146423dfc4f79f5d694cffd948d
imgomez0127 <ianm0127@gmail.com>
2020-08-29 07:10:24 -0400

    Had to do android build stuff and also added react-native-fs loading and storing of shipments so that I could add shipments

--- app/components/AddShipment.js
+++ app/components/AddShipment.js
@@ -1,5 +1,38 @@
 import React, { useState }from 'react'
 import { Button, StyleSheet, View, Text, TextInput} from 'react-native'
+import rnfs from 'react-native-fs'
+
+var shipmentsPath = rnfs.DocumentDirectoryPath + '/shipments.json';
+
+const loadShipmentDetails = async () => {
+  let shipmentsText = rnfs.readDir(shipmentsPath)
+                        .then((result) => {
+                          return Promise.all(rnfs.stat(result[0].path), result[0].path)
+                        })
+                        .then((statResult) => {
+                          if(statResult[0].isFile){
+                            return rnfs.readFile(statResult[1], 'utf8')
+                          }
+                          return "{}"
+                        });
+  shipmentsText = await shipmentsText;
+  return JSON.parse(shipmentsText);
+}
+
+const saveShipmentDetails = async (newShipment) => {
+  let shipments = await loadShipmentDetails();
+  shipments.push(newShipment);
+  rnfs.writeFile(shipmentsPath, JSON.stringify(shipments), 'utf8')
+    .then((success) => console.log("File was written"))
+    .catch((err) =>
+      console.log(err)
+    );
+}
+
+const test = async (newShipment) => {
+  console.log(newShipment)
+}
+
 const AddShipment = (props) => {
   let { navigation } = props;
   let [name, setName] = useState("");
@@ -9,7 +42,7 @@ const AddShipment = (props) => {
     name: "Urban Outfitters",
     tracking_code: "EZ4000000004",
     carrier: "UPS"
-  }
+  };
   return(
     <View style={styles.container}>
       <Text>Shipment Name</Text>
@@ -25,9 +58,12 @@ const AddShipment = (props) => {
                  onChangeText={text => setCarrier(text)}
                  value={carrier}/>
 
+      <Button title="Add Shipment" onPress={() => {
+        newShipment = {name, tracking_code: trackingCode, carrier};
+        test(newShipment);
+      }}/>
       <Button title="Goto Details" onPress={() => navigation.navigate('ShippingDetails', testDetails)}/>
     </View>
-
   )
 }
 
